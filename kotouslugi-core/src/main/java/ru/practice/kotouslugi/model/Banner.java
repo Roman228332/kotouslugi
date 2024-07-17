e26be57cf19707f8b46b3b9de41c48ee9b7584c2
natalia.tarantseva <natalia.tarantseva@rtlabs.ru>
2024-07-17 10:24:45 +0300

    Back

--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/model/Banner.java
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/model/Banner.java
@@ -1,10 +1,10 @@
 package ru.practice.kotouslugi.model;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
